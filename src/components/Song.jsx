bbf559a07c710947fed25b5ef59291c00eba3747
Patrick <parkpatrick1@gmail.com>
2025-06-08 02:11:16 +0300

    Edit (UI): Move index link below tab section in song page

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -14,24 +14,28 @@ export default function Song() {
 
   return (
     <>
-      <div className='lyricsContainer'>{songResult.lyrics ?? '[instrumental]'}</div>
-      <div style={{ marginTop: '2em' }}>
+      <div className='lyricsContainer'>
+        {songResult.lyrics ?? '[instrumental]'}
+      </div>
+
+      <div className='tabsContainer'>
+        {songResult.tabs?.map((t, i) => (
+          <a href={t.url} key={i}>
+            <p className='songItemLink' key={i}>
+              <img
+                className='PdfDocIconImg'
+                alt='PDF document icon'
+                src='/img/pdf-doc-icon.png'
+              />
+              {t.desc}
+            </p>
+          </a>
+        ))}
+      </div>
+
+      <div className='backToIndexDiv'>
         <Link to='/'>← Index</Link>
       </div>
-      <div style={{ height: '3em' }}></div>
-
-      {songResult.tabs?.map((t, i) => (
-        <a href={t.url} key={i}>
-          <p className='songItemLink' key={i}>
-            <img
-              className='PdfDocIconImg'
-              alt='PDF document icon'
-              src='/img/pdf-doc-icon.png'
-            />
-            {t.desc}
-          </p>
-        </a>
-      ))}
     </>
   );
 }
